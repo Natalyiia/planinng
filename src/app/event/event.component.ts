3ef5abd8eebbdc69447fde692919439f95317d4e
Nataliya <nata.dzordzhevich.98@gmail.com>
2021-07-28 11:07:44 +0300

    Styled data and time input

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -9,6 +9,16 @@ import {EventList} from "../app.component";
 export class EventComponent implements OnInit {
   @Input() event:EventList={title:'',type:'',id:0, trainers:[]}
   selectTrainer:any={}
+  date:any
+  time:any
+  setDate(event:any){
+    //console.log(event.target.value)
+    this.date=event.target.value
+  }
+  setTime(event:any){
+   // console.log(event.target.value)
+    this.time=event.target.value
+  }
   constructor() { }
 
   ngOnInit(): void {
